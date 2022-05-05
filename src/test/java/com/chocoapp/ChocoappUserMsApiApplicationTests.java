fe78316d97df5317cee656633cea5387ede7ce7e
Arun S <101079723+arun-gits@users.noreply.github.com>
2022-05-05 17:16:13 +0530

    updated

---
+++ src/test/java/com/chocoapp/ChocoappUserMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocoappUserMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
